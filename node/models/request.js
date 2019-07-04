b0b3f391b592a85f40f28a68cacaf78b4d30a8f6
WagdyUsrey <wagdy.usrey.official@gmail.com>
2019-07-04 11:42:48 +0200

    accept reguest commit

---
+++ node/models/request.js
@@ -0,0 +1,23 @@
+/*jslint node: true*/
+'use strict';
+
+var TimestampsPlugin = require('mongoose-timestamp');
+var backend = require('beamjs').backend();
+var model = backend.model();
+
+module.exports.request = model({
+
+    name: "x"
+}, {
+        requester: {
+            
+            id: Number,
+            name: String
+        },
+        text: text,
+        priority: Number,
+        accepted: Boolean
+    }, [TimestampsPlugin]);
+
+
+
